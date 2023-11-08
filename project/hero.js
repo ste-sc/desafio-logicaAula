d2817fe6ef497c54250a2b824d768b814a50b1f0
ste-sc <stephanie-cardoso@outlook.com>
2023-11-08 14:52:38 -0300

    primeiro commit

---
+++ project/hero.js
@@ -0,0 +1,12 @@
+//variable check
+let heroName = "Stella"
+let heroXP = 1479
+let level = ""
+
+//condition and operation check
+ if (heroXP >=1001 && heroXP <=2000) {
+level = "Bronze"
+}
+
+//output check
+console.log("O Herói de nome " + heroName + " está no nível " + level)
---
+++ project/surpresa.js
@@ -0,0 +1,9 @@
+let poke1 = "POOCHYENA"
+let pokelevel = 3
+let fraseSucess = " foi cadastrado com sucesso!"
+
+console.log (poke1 + fraseSucess)
+
+let pokename = ["dragronite, dragonite; poochyena; wurmple"]
+
+console.log (pokename)
